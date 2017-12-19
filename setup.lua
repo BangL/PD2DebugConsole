3fb22c967d8e2240c97d3d264d2686cb133b1a91
BangL <henno.rickowski@gmail.com>
2017-12-20 00:49:24 +0100

    cleanup, linebreaks

--- setup.lua
+++ setup.lua
@@ -2,7 +2,7 @@ dofile(ModPath .. "DebugConsole.lua")
 
 local init_managers_original = Setup.init_managers
 
-function Setup:init_managers(managers, ...)
-	init_managers_original(self, managers, ...)
+function Setup:init_managers(...)
+	init_managers_original(self, ...)
 	con = DebugConsole:new()
 end
